d4b7ff76c260ed996e0be2983be2369276b23468
Thibaud Colas <thibaudcolas@gmail.com>
2020-06-29 01:24:30 +0100

    Add Heroku config for Postgres

--- django_admin_tests/settings.py
+++ django_admin_tests/settings.py
@@ -63,12 +63,19 @@ WSGI_APPLICATION = 'django_admin_tests.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if os.environ.get('DATABASE_URL'):
+    import dj_database_url
+
+    DATABASES = {
+        'default':  dj_database_url.config()
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
