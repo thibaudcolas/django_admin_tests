0b5116ad9344350dbecc5bdfe6bd967bd9203e80
Thibaud Colas <thibaudcolas@gmail.com>
2020-07-12 23:12:03 +0100

    Improve reports with issues breakdown and anchor links

--- pa11y/report.js
+++ pa11y/report.js
@@ -22,6 +22,16 @@ const issuesByScenario = issues.reduce((by, issue) => {
 }, {});
 
 /**
+ * See https://stackoverflow.com/a/1054862/1798491.
+ */
+const slug = (str) =>
+  str
+    .toLowerCase()
+    .replace(/ /g, "-")
+    .replace(/[-]+/g, "-")
+    .replace(/[^\w-]+/g, "");
+
+/**
  * String concatenation disguised as lit-html, for syntax highlighting purposes.
  */
 const html = (strings, ...values) => {
@@ -31,21 +41,78 @@ const html = (strings, ...values) => {
   }, "");
 };
 
+const githubCorner = html`
+  <a
+    href="https://github.com/thibaudcolas/django_admin_tests"
+    class="github-corner"
+    aria-label="View source on GitHub"
+    ><svg
+      width="80"
+      height="80"
+      viewBox="0 0 250 250"
+      style="fill:#151513; color:#fff; position: absolute; top: 0; border: 0; right: 0;"
+      aria-hidden="true"
+      focusable="false"
+    >
+      <path d="M0,0 L115,115 L130,115 L142,142 L250,250 L250,0 Z"></path>
+      <path
+        d="M128.3,109.0 C113.8,99.7 119.0,89.6 119.0,89.6 C122.0,82.7 120.5,78.6 120.5,78.6 C119.2,72.0 123.4,76.3 123.4,76.3 C127.3,80.9 125.5,87.3 125.5,87.3 C122.9,97.6 130.6,101.9 134.4,103.2"
+        fill="currentColor"
+        style="transform-origin: 130px 106px;"
+        class="octo-arm"
+      ></path>
+      <path
+        d="M115.0,115.0 C114.9,115.1 118.7,116.5 119.8,115.4 L133.7,101.6 C136.9,99.2 139.9,98.4 142.2,98.6 C133.8,88.0 127.5,74.4 143.8,58.0 C148.5,53.4 154.0,51.2 159.7,51.0 C160.3,49.4 163.2,43.6 171.4,40.1 C171.4,40.1 176.1,42.5 178.8,56.2 C183.1,58.6 187.2,61.8 190.9,65.4 C194.5,69.0 197.7,73.2 200.1,77.6 C213.8,80.2 216.3,84.9 216.3,84.9 C212.7,93.1 206.9,96.0 205.4,96.6 C205.1,102.4 203.0,107.8 198.3,112.5 C181.9,128.9 168.3,122.5 157.7,114.1 C157.9,116.9 156.7,120.9 152.7,124.9 L141.0,136.5 C139.8,137.7 141.6,141.9 141.8,141.8 Z"
+        fill="currentColor"
+        class="octo-body"
+      ></path></svg></a
+  ><style>
+    .github-corner:hover .octo-arm {
+      animation: octocat-wave 560ms ease-in-out;
+    }
+    @keyframes octocat-wave {
+      0%,
+      100% {
+        transform: rotate(0);
+      }
+      20%,
+      60% {
+        transform: rotate(-25deg);
+      }
+      40%,
+      80% {
+        transform: rotate(10deg);
+      }
+    }
+    @media (max-width: 500px) {
+      .github-corner:hover .octo-arm {
+        animation: none;
+      }
+      .github-corner .octo-arm {
+        animation: octocat-wave 560ms ease-in-out;
+      }
+    }
+  </style>
+`;
+
 const Scenario = (scenario) => {
   const { category, label, path, states } = scenario;
   const fullLabel = `${category} – ${label}`;
   const issues = issuesByScenario[fullLabel] || [];
+  const axeIssues = issues.filter((i) => i.runner === "axe");
+  const htmlcsIssues = issues.filter((i) => i.runner === "htmlcs");
 
   return html`
     <div style="margin-left: 3rem;">
       <div style="display: grid; grid-template-columns: 1fr 1fr;">
         <div>
-          <h3
-            id="scenario-${encodeURIComponent(
-              category
-            )}%20–%20${encodeURIComponent(label)}"
-          >
+          <h3 id="scenario-${slug(category)}-${slug(label)}">
             ${label}
+            <a
+              href="#scenario-${slug(category)}-${slug(label)}"
+              aria-hidden="true"
+              >#</a
+            >
           </h3>
           <p>
             Path:
@@ -62,9 +129,27 @@ const Scenario = (scenario) => {
             <pre><code>${JSON.stringify(scenario, null, 2)}</code></pre>
           </details>
           <details>
-            <summary>Issues: ${issues.length}</summary>
+            <summary>Axe issues: ${axeIssues.length}</summary>
             <ul>
-              ${issues
+              ${axeIssues
+                .map(
+                  (issue) => html`
+                    <li>
+                      <p>
+                        <code>${issue.runner} ${issue.code}</code>:
+                        ${issue.message}
+                      </p>
+                      <p>${issue.selector}</p>
+                    </li>
+                  `
+                )
+                .join("")}
+            </ul>
+          </details>
+          <details>
+            <summary>HTML_CS: ${htmlcsIssues.length}</summary>
+            <ul>
+              ${htmlcsIssues
                 .map(
                   (issue) => html`
                     <li>
@@ -83,6 +168,7 @@ const Scenario = (scenario) => {
         <a
           href="screenshots/${encodeURIComponent(fullLabel)}.png"
           aria-label="Open screenshot of ${fullLabel}"
+          style="display: block; max-height: 500px; overflow: auto;"
         >
           <img
             src="screenshots/${encodeURIComponent(fullLabel)}.png"
@@ -111,8 +197,11 @@ const Scenario = (scenario) => {
 
 const Category = (category, i) => {
   return html`
-    <section aria-labelledby="category-${encodeURIComponent(category)}">
-      <h2 id="category-${encodeURIComponent(category)}">${category}</h2>
+    <section aria-labelledby="category-${slug(category)}">
+      <h2 id="category-${slug(category)}">
+        ${category}
+        <a href="#category-${slug(category)}" aria-hidden="true">#</a>
+      </h2>
       ${scenariosByCategory[category].map(Scenario).join("")}
     </section>
   `;
@@ -120,6 +209,9 @@ const Category = (category, i) => {
 
 const OverviewRow = (scenarioLabel) => {
   const issues = issuesByScenario[scenarioLabel];
+  const axeIssues = issues.filter((i) => i.runner === "axe");
+  const htmlcsIssues = issues.filter((i) => i.runner === "htmlcs");
+
   if (issues.length === 0) {
     return html` <tr>
       <td>No issues!</td>
@@ -133,11 +225,22 @@ const OverviewRow = (scenarioLabel) => {
       <td>
         ${label}
       </td>
-      <td>${issues.length}</td>
+      <td>${axeIssues.length}</td>
+      <td>${htmlcsIssues.length}</td>
       <td>
-        <a href="http://localhost:8000${scenario.path}">path</a>,
-        <a href="screenshots/${encodeURIComponent(screenshot)}">screenshot</a>,
-        <a href="lighthouse/${encodeURIComponent(lighthouseReport)}"
+        <a
+          href="http://localhost:8000${scenario.path}"
+          aria-label="path - ${label}"
+          >path</a
+        >,
+        <a
+          href="screenshots/${encodeURIComponent(screenshot)}"
+          aria-label="screenshot - ${screenshot}"
+          >screenshot</a
+        >,
+        <a
+          href="lighthouse/${encodeURIComponent(lighthouseReport)}"
+          aria-label="lighthouse - ${lighthouseReport}"
           >lighthouse</a
         >
       </td>
@@ -171,7 +274,7 @@ const report = html`
       </style>
     </head>
     <body>
-      <main>
+      <header>
         <h1>Django admin accessibility tests</h1>
         <p>
           Automated accessibility tests from
@@ -180,13 +283,24 @@ const report = html`
           >.
         </p>
         <p>
-          Generated: <time>${new Date().toISOString().replace("T", " ")}</time>
+          Generated:
+          <a href="https://travis-ci.com/github/thibaudcolas/django_admin_tests"
+            ><time>${new Date().toISOString().replace("T", " ")}</time></a
+          >
         </p>
         <p>
-          All issues: <a href="pa11y.json"><code>pa11y.json</code></a>
+          All issues:
+          <a href="pa11y.json"><code>pa11y.json</code> (large file)</a>
         </p>
-        <p>Running with Axe and HTML CodeSniffer via Pa11y, and Lighthouse.</p>
-        <h2 id="overview">Overview</h2>
+        <p>
+          Running with Axe and HTML CodeSniffer via
+          <a href="https://pa11y.org/">Pa11y</a>, and Lighthouse.
+        </p>
+      </header>
+      <main>
+        <h2 id="overview">
+          Overview <a href="#overview" aria-hidden="true">#</a>
+        </h2>
         <table>
           <caption>
             Overview of test results
@@ -194,7 +308,8 @@ const report = html`
           <thead>
             <tr>
               <th>Scenario</th>
-              <th>Issues</th>
+              <th>Axe issues</th>
+              <th>HTML_CS issues</th>
               <th>Links</th>
             </tr>
           </thead>
@@ -204,6 +319,9 @@ const report = html`
         </table>
         ${categories.map(Category).join("")}
       </main>
+      <footer>
+        ${githubCorner}
+      </footer>
     </body>
   </html>
 `;
