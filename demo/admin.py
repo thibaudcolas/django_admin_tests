a4376f79a5ddf6b288a420f660db0262764ba4e3
Thibaud Colas <thibaudcolas@gmail.com>
2024-01-21 01:37:48 +0000

    Fix ModelAdmin registration

--- demo/admin.py
+++ demo/admin.py
@@ -1,51 +1,19 @@
 from django.apps import apps
 from django.contrib import admin, messages
+from django.contrib.admin.models import LogEntry
+from django.contrib.auth.models import Group, Permission, User
+from django.contrib.auth.admin import GroupAdmin, UserAdmin
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.flatpages.models import FlatPage
+from django.contrib.redirects.models import Redirect
+from django.contrib.sessions.models import Session
+from django.contrib.sites.models import Site
+from django.contrib.flatpages.admin import FlatPageAdmin
 from django.shortcuts import render
 from django.urls import path
 from django.utils.version import get_version
 
 from demo.models import Artist, Release, ReleaseTrack, Track
-
-
-class ReleaseInline(admin.StackedInline):
-    model = Release
-
-
-class ReleaseTrackInline(admin.TabularInline):
-    fields = ["track_number", "track"]
-    model = ReleaseTrack
-
-
-class ArtistAdmin(admin.ModelAdmin):
-    inlines = [ReleaseInline]
-    readonly_fields = ["id"]
-    search_fields = ["name"]
-    list_per_page = 100
-
-
-class ReleaseAdmin(admin.ModelAdmin):
-    autocomplete_fields = ["artist"]
-    date_hierarchy = "release_date"
-    inlines = [ReleaseTrackInline]
-    list_display = ["title", "artist", "type", "release_date"]
-    list_editable = ["type", "release_date"]
-    list_filter = ["type"]
-    readonly_fields = ["id"]
-    search_fields = ["title", "artist__name"]
-    list_per_page = 100
-
-
-class ReleaseTrackAdmin(admin.ModelAdmin):
-    autocomplete_fields = ["release", "track"]
-    search_fields = ["release__title", "track__title", "release__artist__name"]
-    list_per_page = 100
-
-
-class TrackAdmin(admin.ModelAdmin):
-    readonly_fields = ["id"]
-    search_fields = ["artists__name", "title"]
-    list_per_page = 50
-
 
 class MyAdminSite(admin.AdminSite):
     site_header = f"Django administration {get_version()}"
@@ -70,10 +38,57 @@ class MyAdminSite(admin.AdminSite):
 
 
 admin_site = MyAdminSite(name="myadmin")
-models = apps.get_models()
 
-for model in models:
-    try:
-        admin_site.register(model)
-    except admin.sites.AlreadyRegistered:
-        pass
+admin_site.register(Group, GroupAdmin)
+admin_site.register(User, UserAdmin)
+admin_site.register(Permission)
+admin_site.register(LogEntry)
+admin_site.register(FlatPage, FlatPageAdmin)
+admin_site.register(Redirect)
+admin_site.register(Session)
+admin_site.register(Site)
+admin_site.register(ContentType)
+
+
+class ReleaseInline(admin.StackedInline):
+    model = Release
+
+
+class ReleaseTrackInline(admin.TabularInline):
+    fields = ["track_number", "track"]
+    model = ReleaseTrack
+
+
+@admin.register(Artist, site=admin_site)
+class ArtistAdmin(admin.ModelAdmin):
+    inlines = [ReleaseInline]
+    readonly_fields = ["id"]
+    search_fields = ["name"]
+    list_per_page = 100
+
+
+@admin.register(Release, site=admin_site)
+class ReleaseAdmin(admin.ModelAdmin):
+    autocomplete_fields = ["artist"]
+    date_hierarchy = "release_date"
+    inlines = [ReleaseTrackInline]
+    list_display = ["title", "artist", "type", "release_date"]
+    list_editable = ["type", "release_date"]
+    list_filter = ["type"]
+    readonly_fields = ["id"]
+    search_fields = ["title", "artist__name"]
+    list_per_page = 100
+
+
+@admin.register(ReleaseTrack, site=admin_site)
+class ReleaseTrackAdmin(admin.ModelAdmin):
+    autocomplete_fields = ["release", "track"]
+    search_fields = ["release__title", "track__title", "release__artist__name"]
+    list_per_page = 100
+
+
+@admin.register(Track, site=admin_site)
+class TrackAdmin(admin.ModelAdmin):
+    readonly_fields = ["id"]
+    search_fields = ["artists__name", "title"]
+    list_per_page = 50
