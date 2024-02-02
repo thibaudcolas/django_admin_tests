767722ce20b84fbbdec2036162363839bd88be98
Thibaud Colas <thibaudcolas@gmail.com>
2024-02-02 10:12:54 +0100

    Add new error pages

--- snapshot/backup.sh
+++ snapshot/backup.sh
@@ -27,6 +27,10 @@ wget --no-host-directories -P ./django_admin_tests --mirror --reject-regex "(.*)
 wget --no-host-directories -P ./django_admin_tests/django_admin_tests $HOST/django_admin_tests/
 wget --content-on-error --no-host-directories -P ./django_admin_tests --mirror $HOST/django_admin_tests/$VERSION/$VARIANT/defaults/404/
 wget --content-on-error --no-host-directories -P ./django_admin_tests --mirror $HOST/django_admin_tests/$VERSION/$VARIANT/defaults/500/
+wget --content-on-error --no-host-directories -P ./django_admin_tests --mirror $HOST/django_admin_tests/$VERSION/$VARIANT/defaults/500-template-missing/
+wget --content-on-error --no-host-directories -P ./django_admin_tests --mirror $HOST/django_admin_tests/$VERSION/$VARIANT/defaults/500-template-broken/
+wget --content-on-error --no-host-directories -P ./django_admin_tests --mirror $HOST/django_admin_tests/$VERSION/$VARIANT/defaults/500-unicode/
+wget --content-on-error --no-host-directories -P ./django_admin_tests --mirror $HOST/django_admin_tests/$VERSION/$VARIANT/defaults/500-plain-text/
 wget --content-on-error --no-host-directories -P ./django_admin_tests --mirror $HOST/django_admin_tests/$VERSION/$VARIANT/defaults/403/
 wget --content-on-error --no-host-directories -P ./django_admin_tests --mirror $HOST/django_admin_tests/$VERSION/$VARIANT/defaults/400/
 wget --content-on-error --no-host-directories -P ./django_admin_tests --mirror $HOST/django_admin_tests/$VERSION/$VARIANT/defaults/csrf_failure/
