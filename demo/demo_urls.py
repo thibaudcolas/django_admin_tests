6ded11ea59122214ea32e2baf13d80dd28900e31
Thibaud Colas <thibaudcolas@gmail.com>
2024-02-02 07:45:29 +0100

    Add more variations of error pages

--- demo/demo_urls.py
+++ demo/demo_urls.py
@@ -2,13 +2,15 @@ from pathlib import Path
 from django.contrib import admin
 from django.urls import path, include
 from django.views.debug import default_urlconf
-from django.http import Http404
+from django.http import Http404, HttpResponse
 from django.core.exceptions import PermissionDenied, SuspiciousOperation
 from django.contrib.auth import views as auth_views
+from django.shortcuts import render
 from django.views.static import directory_index
 from django.views.csrf import csrf_failure
 from demo.views import example_form, variant_home
 from demo.admin import admin_site
+
 
 def page_not_found(request):
     request.META = {
@@ -22,6 +24,43 @@ def server_error(request):
     request.META = {
         "SERVER_NAME": "Hello",
         "SERVER_PORT": 1337,
+    }
+    raise TypeError("Test 500 view")
+
+
+def server_error_template_missing(request):
+    request.META = {
+        "SERVER_NAME": "Hello",
+        "SERVER_PORT": 1337,
+    }
+    # Render a "foo" template that does not exist to raise a TemplateDoesNotExist:
+    return render(request, "foo.html")
+
+
+def server_error_template_broken(request):
+    request.META = {
+        "SERVER_NAME": "Hello",
+        "SERVER_PORT": 1337,
+    }
+    return render(request, "demo/broken_template.html")
+
+
+def server_error_unicode(request):
+    request.META = {
+        "SERVER_NAME": "Hello",
+        "SERVER_PORT": 1337,
+    }
+    # Raise a UnicodeError
+    r = HttpResponse()
+    r.headers.__setitem__("føø", "bar")
+
+
+def server_error_plaintext(request):
+    request.META = {
+        "SERVER_NAME": "Hello",
+        "SERVER_PORT": 1337,
+        # Modify the request to not accept text/html
+        "HTTP_ACCEPT": "text/plain",
     }
     raise TypeError("Test 500 view")
 
@@ -53,6 +92,10 @@ urlpatterns = [
     # https://docs.djangoproject.com/en/3.0/ref/views/#error-views
     path("defaults/404/", page_not_found),
     path("defaults/500/", server_error),
+    path("defaults/500-template-missing/", server_error_template_missing),
+    path("defaults/500-template-broken/", server_error_template_broken),
+    path("defaults/500-unicode/", server_error_unicode),
+    path("defaults/500-plain-text/", server_error_plaintext),
     path("defaults/403/", permission_denied),
     path("defaults/400/", bad_request),
     path("defaults/csrf_failure/", csrf_failure),
