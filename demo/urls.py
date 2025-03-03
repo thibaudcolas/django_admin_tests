65281b96338cc8d997bd1b34645b7761b15db93a
Thibaud Colas <thibaudcolas@gmail.com>
2025-03-03 19:42:52 +0000

    Remove i18n patterns for now

--- demo/urls.py
+++ demo/urls.py
@@ -41,28 +41,28 @@ urlpatterns = [
     path("debug_toolbar/", include("debug_toolbar.urls")),
 ]
 
-urlpatterns += i18n_patterns(
-    path(
-        "admin/password_reset/",
-        auth_views.PasswordResetView.as_view(),
-        name="admin_password_reset",
-    ),
-    path(
-        "admin/password_reset/done/",
-        auth_views.PasswordResetDoneView.as_view(),
-        name="password_reset_done",
-    ),
-    path(
-        "reset/<uidb64>/<token>/",
-        auth_views.PasswordResetConfirmView.as_view(),
-        name="password_reset_confirm",
-    ),
-    path(
-        "reset/done/",
-        auth_views.PasswordResetCompleteView.as_view(),
-        name="password_reset_complete",
-    ),
-    path("admin/doc/", include("django.contrib.admindocs.urls")),
-    path("admin/", admin.site.urls),
-    path("congrats/", default_urlconf, name="congrats"),
-)
+# urlpatterns += i18n_patterns(
+#     path(
+#         "admin/password_reset/",
+#         auth_views.PasswordResetView.as_view(),
+#         name="admin_password_reset",
+#     ),
+#     path(
+#         "admin/password_reset/done/",
+#         auth_views.PasswordResetDoneView.as_view(),
+#         name="password_reset_done",
+#     ),
+#     path(
+#         "reset/<uidb64>/<token>/",
+#         auth_views.PasswordResetConfirmView.as_view(),
+#         name="password_reset_confirm",
+#     ),
+#     path(
+#         "reset/done/",
+#         auth_views.PasswordResetCompleteView.as_view(),
+#         name="password_reset_complete",
+#     ),
+#     path("admin/doc/", include("django.contrib.admindocs.urls")),
+#     path("admin/", admin.site.urls),
+#     path("congrats/", default_urlconf, name="congrats"),
+# )
